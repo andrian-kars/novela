2fec8c0bb0b0f0f9229b68a1288c3c7a224a607b
Viktor <yadelskyiviktor@gmail.com>
2022-09-14 15:19:09 +0300

    :sparkles:modal component improvement

--- src/components/common/Modal/index.jsx
+++ src/components/common/Modal/index.jsx
@@ -3,18 +3,11 @@ import s from './Modal.module.scss';
 import { Portal } from 'src/components/common';
 
 export const Modal = ({ closeModalHandler, location, children }) => {
-  const stopPropagetion = (e) => {
-    e.stopPropagation();
-  };
   return (
     <Portal>
-      <div className={s.modalOverlay} onClick={closeModalHandler}>
-        <div
-          className={location === 'right' ? s.modalRightContent : s.modalBottomContent}
-          onClick={(e) => stopPropagetion(e)}
-        >
-          {children}
-        </div>
+      <div className={s.modalOverlay} onClick={closeModalHandler} />
+      <div className={location === 'right' ? s.modalRightContent : s.modalBottomContent}>
+        {children}
       </div>
     </Portal>
   );
